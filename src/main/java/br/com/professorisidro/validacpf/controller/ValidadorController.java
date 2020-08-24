85d91f8d46a31967a708f9d0b7453c8f46c5828e
renateix <renateix@mail.com>
2020-08-24 15:24:51 -0300

    versionando home action

--- src/main/java/br/com/professorisidro/validacpf/controller/ValidadorController.java
+++ src/main/java/br/com/professorisidro/validacpf/controller/ValidadorController.java
@@ -19,4 +19,9 @@ public class ValidadorController {
 		 return "{\"status\": \"invalido\"}";
 	}
 
+	@GetMapping("/")
+	public String index() {
+		
+		 return "{\"mensagem\": \"api itau turma 7c\"}";
+	}
 }
